9ae657ac52385b51f4426b877f5a5996621ed47d
Rexicek1 <johnblecha@seznam.cz>
2018-11-05 18:57:49 +0100

    Přidán Skript na scaleování textur

---
+++ RaptorGame/Assets/Scripts/TextureTilingController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TextureTilingController : MonoBehaviour {
+    public float repeatEvery = 5;
+
+    // Use this for initialization
+    void Start() {
+        gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(gameObject.transform.localScale.x / repeatEvery, 1f);
+    }
+}
